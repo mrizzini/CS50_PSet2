ef14ef15720781686f85a461b26d4db32e2917bb
Matthew Rizzini <matthewrizzini@gmail.com>
2018-04-08 01:36:10 +0000

    different solution using array

--- initials/more/initials.c
+++ initials/more/initials.c
@@ -9,10 +9,16 @@ int main(void)
 
     string fullName = get_string(); // gets name from user
     int strLength = strlen(fullName); // stores length of name
+    char initials[4];
+    int counter = 0;
+    // initials[0] = fullName[counter];
+    // counter++;
 
-    if (isalpha(fullName[0])) // checks if first char in string is alphabetical, if it is, it is printed
+    if (isalpha(fullName[counter])) // checks if first char in string is alphabetical, if it is, it is printed
     {
-        printf("%c", toupper(fullName[0])); // prints upper case
+        // printf("%c", toupper(fullName[0])); // prints upper case
+        initials[counter] = toupper(fullName[counter]);
+        counter++;
     }
 
     for (int i = 0; i < strLength; i++) // for loop to iterate thru length of name
@@ -22,11 +28,14 @@ int main(void)
         {
             if (isalpha(fullName[i + 1])) // checks for character AFTER the space. if it is alphabetical, it prints
             {
-                printf("%c", toupper(fullName[i + 1])); // prints uppercase char after space if it is alphabetical
+                // printf("%c", toupper(fullName[i + 1])); // prints uppercase char after space if it is alphabetical
+                initials[counter] = toupper(fullName[i + 1]);
+                counter++;
             } // ends second IF
         } // ends first IF
 
     } // ends for loop
 
-    printf("\n"); // prints new line
+    initials[counter] = '\0';
+    printf("%s\n", initials); // prints new line
 }
